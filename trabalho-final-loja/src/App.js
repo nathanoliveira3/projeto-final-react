72f98e5d197986f28cd3354cb5eef5a539a7750e
Nathan Oliveira <nathanoliveira3@gmail.com>
2021-06-23 10:24:15 -0300

    criação de página de cadastro

--- trabalho-final-loja/src/App.js
+++ trabalho-final-loja/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Cadastro from './Pages/Cadastro'
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       
-    </div>
+      <Switch>        
+        <Route exact path="/cadastro">
+          <Cadastro />
+        </Route>        
+        
+      </Switch>
+    </BrowserRouter>
   );
 }
 
