56b7ac939b4051de3df0395f0d41b60cd8b95617
bluejynz <danifalk27@gmail.com>
2021-06-24 13:55:01 -0300

    detalhes conta

--- trabalho-final-loja/src/Pages/ContaDetalhes/index.jsx
+++ trabalho-final-loja/src/Pages/ContaDetalhes/index.jsx
@@ -6,7 +6,9 @@ import Titulo from "../../Components/Titulo";
 const ContaDetalhes = () => {
 
     const [cliente, setCliente] = useState({});
-    const history = useHistory()
+    const history = useHistory();
+
+    const [endereco, setEndereco] = useState("");
 
     const editarDados = () => {
         history.push('/conta/editar')
@@ -19,16 +21,13 @@ const ContaDetalhes = () => {
             .then(response => {
                 setCliente(response.data);
                 console.log(response.data);
+                setEndereco(`${response.data.endereco.rua}, ${response.data.endereco.numero} - ${response.data.endereco.bairro}, ${response.data.endereco.cidade} - ${response.data.endereco.estado}, ${response.data.endereco.cep}`);
             });
     }, [])
 
     const excluirConta = () => {
         alert("Tem certeza que deseja excluir sua conta?");
     }
-
-    // const exibirEndereco = () => {
-    //     return `${cliente.endereco.rua}, ${cliente.endereco.numero} - ${cliente.endereco.bairro}, ${cliente.endereco.cidade} - ${cliente.endereco.estado}, ${cliente.endereco.cep}`
-    // }
 
     return (
         <div>
@@ -48,7 +47,7 @@ const ContaDetalhes = () => {
                 <input className="form-control" type="text" value={cliente.telefone} aria-label="Disabled input example" disabled readOnly></input>
                 
                 <label className="form-label">Endereço: </label>
-                <input className="form-control" type="text" value={`${cliente.endereco.rua}, ${cliente.endereco.numero} - ${cliente.endereco.bairro}, ${cliente.endereco.cidade} - ${cliente.endereco.estado}, ${cliente.endereco.cep}`} aria-label="Disabled input example" disabled readOnly></input>
+                <input className="form-control" type="text" value={endereco} aria-label="Disabled input example" disabled readOnly></input>
             </form>
             <button className="btn btn-dark fw-bold d-grid gap-2 col-2 mx-auto my-5" onClick={editarDados}>Editar dados</button>
             <button className="btn btn-danger fw-bold d-grid gap-2 col-2 mx-auto my-5" onClick={excluirConta}>Excluir conta</button>
