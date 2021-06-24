e5cd16f14969311a91564dd0dd44946c78efa62f
Vinicius Gaspar Siqueira <vgsiqueira94@gmail.com>
2021-06-24 11:50:48 -0300

    Alterações na página home

--- trabalho-final-loja/src/Components/CategoriasPopulares/index.jsx
+++ trabalho-final-loja/src/Components/CategoriasPopulares/index.jsx
@@ -1,18 +1,44 @@
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBuilding, faCar, faFutbol, faGamepad, faTshirt, faMobileAlt, faMicrophone, faHammer, faTv, faCouch } from "@fortawesome/free-solid-svg-icons";
+import './estilos.css'
 
 const CategoriasPopulares = () => {
 
     return (
-        <div className="container">
-            <div className="row">
+        <div className="container mt-3">
+            <div className="row rol-cols-5 text-center">
                 <div className="col">
-                    Column
+                    <FontAwesomeIcon icon={faCar} className="fa-2x" /> <p className="">Carros, Motos e Outros</p>
                 </div>
                 <div className="col">
-                    Column
+                    <FontAwesomeIcon icon={faTshirt} className="fa-2x" /><p>Calçados, Roupas e Bolsas</p>
                 </div>
                 <div className="col">
-                    Column
+                    <FontAwesomeIcon icon={faMobileAlt} className="fa-2x" /><p>Celulares e Telefones</p>
+                </div>
+                <div className="col">
+                    <FontAwesomeIcon icon={faMicrophone} className="fa-2x" /><p>Eletrônicos, Áudio e Vídeo</p>
+                </div>
+                <div className="col">
+                    <FontAwesomeIcon icon={faHammer} className="fa-2x" /><p>Ferramentas</p>
+                </div>
+
+            </div>
+            <div className="row rol-cols-5 text-center">
+                <div className="col">
+                    <FontAwesomeIcon icon={faCouch} className="fa-2x" /><p>Casa, Móveis e Decoração</p>
+                </div>
+                <div className="col">
+                    <FontAwesomeIcon icon={faTv} className="fa-2x" /><p>Informática</p>
+                </div>
+                <div className="col">
+                    <FontAwesomeIcon icon={faFutbol} className="fa-2x" /><p>Esportes e Fitness</p>
+                </div>
+                <div className="col">
+                    <FontAwesomeIcon icon={faGamepad} className="fa-2x" /><p>Games</p>
+                </div>
+                <div className="col">
+                    <FontAwesomeIcon icon={faBuilding} className="fa-2x" /><p>Imóveis</p>
                 </div>
             </div>
         </div>
