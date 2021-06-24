1937d62079029427896c98b49569c4fdb0f87b3e
Vinicius Gaspar Siqueira <vgsiqueira94@gmail.com>
2021-06-24 14:54:12 -0300

    Ajuste footer

--- trabalho-final-loja/src/App.js
+++ trabalho-final-loja/src/App.js
@@ -71,8 +71,8 @@ function App() {
 
           </Switch>
 
-          <Footer />
         </div>
+          <Footer />
       </BrowserRouter>
     </div>
   );
--- trabalho-final-loja/src/Components/Footer/index.jsx
+++ trabalho-final-loja/src/Components/Footer/index.jsx
@@ -1,11 +1,9 @@
 import  './estilos.css'
 const Footer = () => {
     return (
-        <div>
             <footer className="text-center">
                 <p className="footer">&copy;Copyright Serratec - Livraria - 2021</p>
             </footer>
-        </div>
     )
 }
 
